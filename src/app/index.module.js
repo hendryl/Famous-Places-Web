a5e540ec85ddcdb339c2a043802fe7bf46dfe24b
Hendry <hendry@icehousecorp.com>
2015-12-18 12:32:16 +0700

    Fix errors

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,16 +4,14 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
-angular.module('famousPlacesWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr', 'ngAudio', 'ngStorage','socket.io', 'peerjs' 'lodash'])
+var lodash = require('lodash');
+
+angular.module('famousPlacesWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'angularScreenfull', 'ui.router', 'ui.bootstrap', 'toastr', 'ngAudio', 'ngStorage', 'btford.socket-io'])
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .constant('_', lodash)
   .config(config)
   .config(routerConfig)
-  .run(runBlock
+  .run(runBlock)
   .controller('MainController', MainController);
