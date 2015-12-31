54f3ce25289725244ff7945559223c7d9cd12cca
Hendry <email.hendry.lee@gmail.com>
2015-12-31 23:58:36 +0700

    Modify players list when player disconnects

--- src/app/lobby/lobby.controller.js
+++ src/app/lobby/lobby.controller.js
@@ -1,9 +1,11 @@
 class LobbyController {
-  constructor($scope, $log, $location, GameService, SocketService) {
+  constructor(_, $scope, toastr, $log, $location, GameService, SocketService) {
     'ngInject';
 
+    this._ = _;
     this.$log = $log;
     this.$scope = $scope;
+    this.toastr = toastr;
     this.GameService = GameService;
     this.SocketService = SocketService;
 
@@ -17,17 +19,37 @@ class LobbyController {
       this.$log.log('success');
 
       this.SocketService.extendedHandler = (message) => {
-        if(message.type === 'join_room') {
+        if (message.type === 'join_room') {
           this.players.push({
             name: message.name,
             id: message.id
           });
-          $scope.$apply( () =>{
+
+          toastr.success(message.name + ' has connected.');
+
+          $scope.$apply(() => {
             $scope.players = this.players;
           });
         }
 
-        //TODO: handle player disconnect
+        if (message.type === 'player_disconnect') {
+          const _ = this._;
+          const index = _.chain(this.players)
+            .map((n) => n.id)
+            .indexOf(message.id)
+            .value();
+
+          if (index != -1) {
+            toastr.warning(this.players[index].name + ' has disconnected.');
+            _.remove(this.players, (n) => n.id === message.id);
+
+            $scope.$apply(() => {
+              $scope.players = this.players;
+            });
+          } else {
+            this.$log.error('Player with id ' + message.id + ' not found!');
+          }
+        }
       };
 
       SocketService.createRoom(this.password);
