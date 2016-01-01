d989f9c05d8a2b4978975267254e0c57bc4e0f9f
Hendry <email.hendry.lee@gmail.com>
2016-01-01 20:05:10 +0700

    Refactor message logic out of constructor

--- src/app/lobby/lobby.controller.js
+++ src/app/lobby/lobby.controller.js
@@ -20,40 +20,48 @@ class LobbyController {
 
       this.SocketService.extendedHandler = (message) => {
         if (message.type === 'join_room') {
-          this.players.push({
-            name: message.name,
-            id: message.id
-          });
-
-          toastr.success(message.name + ' has connected.');
-
-          $scope.$apply(() => {
-            $scope.players = this.players;
-          });
+          this.handleJoinRoomMessage(message);
         }
 
         if (message.type === 'player_disconnect') {
-          const _ = this._;
-          const index = _.chain(this.players)
-            .map((n) => n.id)
-            .indexOf(message.id)
-            .value();
-
-          if (index != -1) {
-            toastr.warning(this.players[index].name + ' has disconnected.');
-            _.remove(this.players, (n) => n.id === message.id);
-
-            $scope.$apply(() => {
-              $scope.players = this.players;
-            });
-          } else {
-            this.$log.error('Player with id ' + message.id + ' not found!');
-          }
+          this.handlePlayerDisconnect(message);
         }
       };
 
       SocketService.createRoom(this.password);
     });
+  }
+
+  handleJoinRoomMessage(message) {
+    this.players.push({
+      name: message.name,
+      id: message.id
+    });
+
+    this.toastr.success(message.name + ' has connected.');
+
+    this.$scope.$apply(() => {
+      this.$scope.players = this.players;
+    });
+  }
+
+  handlePlayerDisconnect(message) {
+    const _ = this._;
+    const index = _.chain(this.players)
+      .map((n) => n.id)
+      .indexOf(message.id)
+      .value();
+
+    if (index != -1) {
+      this.toastr.warning(this.players[index].name + ' has disconnected.');
+      _.remove(this.players, (n) => n.id === message.id);
+
+      this.$scope.$apply(() => {
+        this.$scope.players = this.players;
+      });
+    } else {
+      this.$log.error('Player with id ' + message.id + ' not found!');
+    }
   }
 
   isPlayerConnected(number) {
