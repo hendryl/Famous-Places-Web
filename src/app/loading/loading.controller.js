397a67904b324d152c8325139f5ca65b211fe2a6
Hendry <email.hendry.lee@gmail.com>
2016-01-03 21:10:04 +0700

    Store dependencies and convert lines into methods

--- src/app/loading/loading.controller.js
+++ src/app/loading/loading.controller.js
@@ -1,9 +1,14 @@
 class LoadingController {
-  constructor($log, $scope, $state, $interval) {
+  constructor($log, $scope, $state, $interval, SocketService, GameService) {
     'ngInject';
 
     this.$log = $log;
+    this.$interval = $interval;
+    this.GameService = GameService;
+    this.SocketService = SocketService;
+
     this.waitTime = 5000;
+    this.animTime = 850;
     this.ellipsis = '.';
 
     $scope.$on('server_disconnect', function(event, args) {
@@ -11,11 +16,19 @@ class LoadingController {
       $state.go('home');
     });
 
-    $interval(() => this.changeEllipsis(), 850, 0, true);
+    this.setAnimation();
+    this.setGameAssetChecker(this.waitTime);
+  }
+
+  setAnimation() {
+    this.$interval(() => this.changeEllipsis(), this.animTime, 0, true);
+  }
+
+  setGameAssetChecker(waitTime) {
+    this.$interval(() => this.checkAssets(), waitTime, 1, true);
   }
 
   changeEllipsis() {
-    this.$log.log('change');
     this.ellipsis += '.';
 
     if (this.ellipsis.length > 3) {
