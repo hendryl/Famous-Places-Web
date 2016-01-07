b893815367707947ca19812a1442be8b9f314e76
Hendry <email.hendry.lee@gmail.com>
2016-01-07 23:10:21 +0700

    Complete calculateScore algorithm

--- src/app/services/score.service.js
+++ src/app/services/score.service.js
@@ -7,17 +7,38 @@ class ScoreService {
   }
 
   calculateScore(distance) {
-    if(distance < 0 || distance > 999999) {
-      return 0;
-    } else if(distance < 10) {
-      return this.maxScore;
-    } else if(distance < 10000) {
-      return 100;
-      //TODO: perform cool logarithms
-    } else { // 10001 - 999999
-      return 50;
-      //TODO: perform cool logarithm 2
+    let result = 0;
+
+    if(distance < 0 || distance >= 10000000) {
+      result = 0;
+
+    } else if(distance <= 10) {
+      result = this.maxScore;
+
+    } else if(distance <= 10000) {
+      result = this.maxScore - this.reduceSmall(distance);
+
+    } else if(distance <= 1000000) {
+      result = this.maxScore - this.reduceMedium(distance);
+
+    } else { // 1,000,001 - ???
+      result = this.maxScore - this.reduceLarge(distance);
     }
+
+    result = result < 0 ? 0 : result;
+    return Math.round(result);
+  }
+
+  reduceSmall(distance) {
+    return 14.476 * Math.log(distance) - 33.333;
+  }
+
+  reduceMedium(distance) {
+    return 86.859 * Math.log(distance) - 700;
+  }
+
+  reduceLarge(distance) {
+    return 217.15 * Math.log(distance) - 2500;
   }
 
   padWithZeroes(value) {
