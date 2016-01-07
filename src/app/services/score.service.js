28600aa663a744ed4103f2d28bf4510de77b9aa3
Hendry <hendry@icehousecorp.com>
2016-01-07 14:15:45 +0700

    Create calculateScore method with test

--- src/app/services/score.service.js
+++ src/app/services/score.service.js
@@ -3,10 +3,21 @@ class ScoreService {
     'ngInject';
 
     this._ = _;
+    this.maxScore = 1000;
   }
 
   calculateScore(distance) {
-    
+    if(distance < 0 || distance > 999999) {
+      return 0;
+    } else if(distance < 10) {
+      return this.maxScore;
+    } else if(distance < 10000) {
+      return 100;
+      //TODO: perform cool logarithms
+    } else { // 10001 - 999999
+      return 50;
+      //TODO: perform cool logarithm 2
+    }
   }
 
   padWithZeroes(value) {
