b0d87867cf58b4b04146ba7b7b8ebaadf9c65b61
Hendry <hendry@icehousecorp.com>
2016-01-07 13:23:43 +0700

    Create basic layout

--- src/app/score/score.controller.js
+++ src/app/score/score.controller.js
@@ -1,6 +1,21 @@
 class ScoreController {
-  constructor(SocketService, GameService) {
+  constructor(_, NgMap, toastr, $state, $scope, $log, AudioService, SocketService, GameService) {
     'ngInject';
+
+    this._ = _;
+    this.$log = $log;
+    this.toastr = toastr;
+    this.$state = $state;
+    this.GameService = GameService;
+    this.AudioService = AudioService;
+    this.SocketService = SocketService;
+
+    this.round = GameService.getRound();
+    this.question = GameService.getQuestion(this.round);
+    this.players = GameService.players;
+
+  padWithZeroes(value) {
+    return this._.padLeft(value.toString(), 6, '0');
   }
 }
 
