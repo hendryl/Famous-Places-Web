cd925e31e18cb647e3dcb16eeef035db54dcd909
Hendry <hendry@icehousecorp.com>
2016-01-12 15:29:50 +0700

    Refactor player_create message handler

--- src/app/result/result.controller.js
+++ src/app/result/result.controller.js
@@ -1,17 +1,23 @@
 class ResultController {
-  constructor(SocketService, GameService, toastr, _) {
+  constructor(SocketService, GameService, toastr, _, GameFactory) {
     'ngInject';
 
+    this._ = _;
     this.GameService = GameService;
     this.players = GameService.players;
+    this.toastr = toastr;
 
     SocketService.extendedHandler = (message) => {
       if(message.type === 'player_create') {
-        const player = _.find(GameService.players, (n) => n.id === message.player);
+        this.handlePlayerCreate(message.player);
 
-        toastr.info(player + ' is creating a new game.');
       }
     }
+  }
+
+  handlePlayerCreate(playerId) {
+    const player = this._.find(this.GameService.players, (n) => n.id === playerId);
+    this.toastr.info(player + ' is creating a new game.');
   }
 }
 
