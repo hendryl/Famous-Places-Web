eb2190f385aadbbbe682022ea0e06127e524f0de
Hendry <hendry@icehousecorp.com>
2016-01-12 15:30:47 +0700

    Handle player_select message

--- src/app/result/result.controller.js
+++ src/app/result/result.controller.js
@@ -11,6 +11,8 @@ class ResultController {
       if(message.type === 'player_create') {
         this.handlePlayerCreate(message.player);
 
+      } else if(message.type === 'player_select') {
+        this.handlePlayerSelect(message);
       }
     }
   }
@@ -18,6 +20,29 @@ class ResultController {
   handlePlayerCreate(playerId) {
     const player = this._.find(this.GameService.players, (n) => n.id === playerId);
     this.toastr.info(player + ' is creating a new game.');
+  }
+
+  handlePlayerSelect(message) {
+    const mode_id = message.mode_id;
+
+    this.GameFactory.createGame(mode_id).then((result) => {
+      this.toastr.info('Moving to lobby');
+
+      this.GameService.storeGameData(result.data);
+      this.GameService.retrieveAssets();
+
+      const player = this._.find(this.GameService.players, (n) => n.id === message.player);
+      this.GameService.players = [{
+        name: player.name,
+        id: player.id,
+        score: 0,
+        lastAnswer: null
+      }];
+
+      this.$state.go('lobby');
+    }, (error) => {
+      this.$log.log(error);
+    });
   }
 }
 
