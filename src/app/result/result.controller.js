bccf59e8af6acb86b11891a8899e96fd182e7997
Hendry <hendry@icehousecorp.com>
2016-01-12 13:58:01 +0700

    Create handler for player_create message

--- src/app/result/result.controller.js
+++ src/app/result/result.controller.js
@@ -1,6 +1,17 @@
 class ResultController {
-  constructor() {
+  constructor(SocketService, GameService, toastr, _) {
     'ngInject';
+
+    this.GameService = GameService;
+    this.players = GameService.players;
+
+    SocketService.extendedHandler = (message) => {
+      if(message.type === 'player_create') {
+        const player = _.find(GameService.players, (n) => n.id === message.player);
+
+        toastr.info(player + ' is creating a new game.');
+      }
+    }
   }
 }
 
