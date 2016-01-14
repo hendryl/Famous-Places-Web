af0fe749de98cc027bcaeed668372c398a2be35c
Hendry <hendry@icehousecorp.com>
2016-01-14 17:55:17 +0700

    Make single player results

--- src/app/result/result.controller.js
+++ src/app/result/result.controller.js
@@ -27,32 +27,25 @@ class ResultController {
       score: 1235,
       id: 123123132132,
       lastAnswer: null
+    },
+    {
+      name: 'player 2',
+      score: 3335,
+      id: 12312317712,
+      lastAnswer: null
+    },
+    {
+      name: 'player 3',
+      score: 4235,
+      id: 123123152132,
+      lastAnswer: null
+    },
+    {
+      name: 'player 4',
+      score: 2235,
+      id: 123999132132,
+      lastAnswer: null
     }];
-
-    // this.players = [{
-    //   name: 'player 1',
-    //   score: 1235,
-    //   id: 123123132132,
-    //   lastAnswer: null
-    // },
-    // {
-    //   name: 'player 2',
-    //   score: 3335,
-    //   id: 12312317712,
-    //   lastAnswer: null
-    // },
-    // {
-    //   name: 'player 3',
-    //   score: 4235,
-    //   id: 123123152132,
-    //   lastAnswer: null
-    // },
-    // {
-    //   name: 'player 4',
-    //   score: 2235,
-    //   id: 123999132132,
-    //   lastAnswer: null
-    // }];
 
     this.sortWinners();
     this.$log.log(this.winners);
@@ -64,10 +57,58 @@ class ResultController {
       .each(p => {
         p.number = counter;
         counter += 1;
+        p.rating = this.getRating(p.score);
+        p.flavor = this.getFlavorText(p.score);
       })
       .sortBy(p => p.score)
       .value()
       .reverse();
+  }
+
+  getRating(score) {
+    if(score < 1000) {
+      return 'E';
+
+    } else if(score < 2000) {
+      return 'D';
+
+    } else if(score < 3000) {
+      return 'C';
+
+    } else if(score < 4000) {
+      return 'B';
+
+    } else if(score < 4900) {
+      return 'A';
+
+    } else return 'S';
+  }
+
+  getFlavorText(score) {
+    if(score < 100) {
+      return '........';
+
+    } else if(score < 1000) {
+      return 'Try harder!';
+
+    } else if(score < 2000) {
+      return 'Try harder!';
+
+    } else if(score < 3000) {
+      return 'Not bad! Play some more and get B rating!';
+
+    } else if(score < 4000) {
+      return 'Well Done! Try to get A rating!';
+
+    } else if(score < 4900) {
+      return 'Awesome!! Just a little more for S rating!';
+
+    } else if(score < 5000) {
+      return 'Awesome!! Just a bit more for the perfect score!';
+
+    } else {
+      return 'PERFECT SCOREEEE!!! AMAZINGGGG!!!';
+    }
   }
 
   handlePlayerCreate(playerId) {
